f3559f2b6d0b27b50793976e38f67dfbc4b307e8
Ubuntu <mohamedafham@myVMT4.uvfdy3dr3v1erglseici3kqxxe.bx.internal.cloudapp.net>
2021-09-16 06:59:12 +0000

    Download Code Change

--- download_data.sh
+++ download_data.sh
@@ -2,7 +2,7 @@ wget https://coe.northeastern.edu/Research/AClab/SLP_VIPCup/train.zip
 wget https://coe.northeastern.edu/Research/AClab/SLP_VIPCup/valid.zip
 wget https://coe.northeastern.edu/Research/AClab/SLP_VIPCup/test1.zip
 
-unzip train.zip data/SLP_VIPCup/
-unzip valid.zip data/SLP_VIPCup/
-unzip test1.zip data/SLP_VIPCup/
+unzip train.zip -d data/SLP_VIPCup/
+unzip valid.zip -d data/SLP_VIPCup/
+unzip test1.zip -d data/SLP_VIPCup/
 
